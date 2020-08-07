8250da080f330e69fd299c135be52e3554aeb3d5
Adam Neumann <noizwaves@users.noreply.github.com>
2020-08-07 15:35:42 -0600

    Refactor backend with async await plus other structural improvements

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const cors = require('cors')
 const process = require('process')
 const { resolve } = require('path');
 const { readdir, writeFile } = require('fs').promises;
@@ -8,7 +7,6 @@ const sha1 = require('sha1')
 const path = require('path')
 const imageThumbnail = require('image-thumbnail');
 
-const app = express()
 
 //
 // File i/o
@@ -22,14 +20,23 @@ async function getFiles(dir) {
   return Array.prototype.concat(...files);
 }
 
+
 //
 // Photos
 //
 const PHOTO_REGEX = new RegExp("^(?<year>[\\d]{4})\\/(?<month>[\\d]{1,2})\\/(?<day>[\\d]{1,2})\\/(?<filename>.*\.(jpg|png))$", 'i')
 
 class Photo {
+    static get PATH_PATTERN() {
+        return PHOTO_REGEX
+    }
+
     constructor(relativePath) {
-        const match = PHOTO_REGEX.exec(relativePath)
+        const match = Photo.PATH_PATTERN.exec(relativePath)
+
+        if (!match) {
+            throw new Error(`Photo file path does not match expected pattern`)
+        }
         
         this.year = parseInt(match.groups.year)
         this.month = parseInt(match.groups.month)
@@ -45,81 +52,102 @@ class Photo {
 }
 
 
-const galleryPath = process.env.GALLERY_PATH
+//
+// Thumbnails
+//
+const getThumbnailAbsolutePath = (thumbnailsPath, photo) => {
+    return path.join(thumbnailsPath, photo.thumbnailRelativePath)
+}
 
-let photos = []
-getFiles(galleryPath)
-    .then(filePaths => {
-        photos = filePaths
+const generateThumbnailFile = async (galleryPath, thumbnailsPath, photo) => {
+    const finalPath = getThumbnailAbsolutePath(thumbnailsPath, photo)
+    if (fs.existsSync(finalPath)) {
+        return Promise.resolve()
+    }
+
+    const imagePath = path.join(galleryPath, photo.relativePath)
+    const options = { percentage: 25 }
+
+    try {
+        const thumbnail = await imageThumbnail(imagePath, options)
+        return writeFile(finalPath, thumbnail)
+    } catch(err) {
+        console.log(`Error with ${imagePath}: ${err.message}`)
+        return Promise.resolve()
+    }
+}
+
+
+//
+// Application state
+//
+const loadApplicationState = async (galleryPath, thumbnailsPath) => {
+    const files = await getFiles(galleryPath)
+
+    const photos =
+        files
             .map(absPath => absPath.substring(galleryPath.length))
             .sort()
             .reverse()
             .filter(path => PHOTO_REGEX.test(path))
             .map(path => new Photo(path))
 
-        console.log(`Found ${photos.length} photos, generating thumbnails...`)
+    console.log(`Found ${photos.length} photos, generating thumbnails...`)
 
-        // TODO: handle the async return from this properly...
-        photos.forEach(generateThumbnail)
+    await Promise.all(photos.map(p => generateThumbnailFile(galleryPath, thumbnailsPath, p)))
+    console.log('Thumbnails generated')
 
-        console.log('Thumbnails generated')
+    return Promise.resolve({ photos, galleryPath, thumbnailsPath })
+}
+
+
+//
+// Application
+//
+const buildApplication = (app, { galleryPath, thumbnailsPath, photos }) => {
+    app.use('/raw', express.static(galleryPath))
+
+    app.use('/thumbnail', express.static(thumbnailsPath))
+
+    app.get('/api/photos', (req, res) => {
+        const photoJson = photos.map(p => {
+            return {
+                filename: p.filename,
+                rawUrl: `/raw/${p.relativePath}`,
+                thumbnailUrl: `/thumbnail/${p.thumbnailRelativePath}`,
+                date: { year: p.year, month: p.month, day: p.day }
+            }
+        })
+
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify(photoJson))
     })
 
+    app.use('/', express.static(path.join(__dirname, '../frontend/build/')))
+}
+
+
 //
-// Thumbnails
+// Configuration
 //
+const galleryPath = process.env.GALLERY_PATH
 const thumbnailsPath = process.env.THUMBNAILS_PATH
-
-function thumbnailPath(photo) {
-    return path.join(thumbnailsPath, photo.thumbnailRelativePath)
-}
-
-async function generateThumbnail(photo) {
-    const finalPath = thumbnailPath(photo)
-    if (fs.existsSync(finalPath)) {
-        return
-    }
-
-    const imagePath = path.join(galleryPath, photo.relativePath)
-    // const options = { height: 200, width: 200 }
-    const options = { percentage: 25 }
-    const thumbnail = await imageThumbnail(imagePath, options)
-    await writeFile(finalPath, thumbnail)
-}
-
-//
-// Web
-//
-const corsOptions = {
-    origin: 'http://localhost:3000',
-    optionsSuccessStatus: 200
-}
-
-// Serve raw gallery
-app.use('/raw', express.static(galleryPath))
-
-// Serve thumbnail images
-app.use('/thumbnail', express.static(thumbnailsPath))
-
-// Photos API
-app.get('/api/photos', cors(corsOptions), (req, res) => {
-    const photoJson = photos.map(p => {
-        return {
-            filename: p.filename,
-            rawUrl: `/raw/${p.relativePath}`,
-            thumbnailUrl: `/thumbnail/${p.thumbnailRelativePath}`,
-            date: { year: p.year, month: p.month, day: p.day }
-        }
-    })
-
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(photoJson))
-})
-
-// Serve front end
-app.use('/', express.static(path.join(__dirname, '../frontend/build/')))
-
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`)
-})
+
+
+//
+// Bootstrap the application
+//
+loadApplicationState(galleryPath, thumbnailsPath)
+    .then(state => {
+        const app = express()
+
+        buildApplication(app, state)
+
+        app.listen(PORT, () => {
+            console.log(`Server is listening on port ${PORT}`)
+        })
+    })
+    .catch(err => {
+        console.log(`ERROR loading application: ${err.message}`)
+    })
