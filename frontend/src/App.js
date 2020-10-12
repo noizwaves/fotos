de52e6041de11170b44206fa1858bf84838c3ec8
Adam Neumann <noizwaves@users.noreply.github.com>
2020-10-11 21:47:55 -0600

    Add Calendar gallery type

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,10 @@
 import React from 'react';
 import axios from 'axios'
 import {DateTime} from 'luxon';
-import {
-  List,
-  AutoSizer,
-  CellMeasurer,
-  CellMeasurerCache,
-} from 'react-virtualized'
+import {AutoSizer, CellMeasurer, CellMeasurerCache, List,} from 'react-virtualized'
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {
-  faFolder,
-  faPlus,
-  faMinus,
-  faCaretRight,
-  faCaretDown,
-  faAngleDoubleLeft,
-  faImages
-} from '@fortawesome/free-solid-svg-icons'
-import {
-  Switch,
-  Route,
-  Link,
-  NavLink,
-  useParams,
-  useHistory
-} from 'react-router-dom';
+import {faCaretDown, faCaretRight, faFolder, faImages, faMinus, faPlus} from '@fortawesome/free-solid-svg-icons'
+import {Link, NavLink, Route, Switch, useHistory, useParams} from 'react-router-dom';
 
 import './reset.css'
 import './App.css'
@@ -37,6 +17,117 @@ const groupBy = (keyFunc, items) => {
 
   return Object.keys(hash).map(key => ({key: key, items: hash[key]}));
 }
+
+/**
+ * Returns a zero-based index for first day of the week, as used by the specified locale
+ * e.g. Sunday (returns 0), or Monday (returns 1)
+ *
+ * @param locale
+ * @returns {number}
+ */
+const getFirstDayOfWeek = (locale) => {
+  // from http://www.unicode.org/cldr/data/common/supplemental/supplementalData.xml:supplementalData/weekData/firstDay
+  const firstDay = {/*default is 1=Monday*/
+    "bd": 5,
+    "mv": 5,
+    "ae": 6,
+    "af": 6,
+    "bh": 6,
+    "dj": 6,
+    "dz": 6,
+    "eg": 6,
+    "iq": 6,
+    "ir": 6,
+    "jo": 6,
+    "kw": 6,
+    "ly": 6,
+    "ma": 6,
+    "om": 6,
+    "qa": 6,
+    "sa": 6,
+    "sd": 6,
+    "sy": 6,
+    "ye": 6,
+    "ag": 0,
+    "ar": 0,
+    "as": 0,
+    "au": 0,
+    "br": 0,
+    "bs": 0,
+    "bt": 0,
+    "bw": 0,
+    "by": 0,
+    "bz": 0,
+    "ca": 0,
+    "cn": 0,
+    "co": 0,
+    "dm": 0,
+    "do": 0,
+    "et": 0,
+    "gt": 0,
+    "gu": 0,
+    "hk": 0,
+    "hn": 0,
+    "id": 0,
+    "ie": 0,
+    "il": 0,
+    "in": 0,
+    "jm": 0,
+    "jp": 0,
+    "ke": 0,
+    "kh": 0,
+    "kr": 0,
+    "la": 0,
+    "mh": 0,
+    "mm": 0,
+    "mo": 0,
+    "mt": 0,
+    "mx": 0,
+    "mz": 0,
+    "ni": 0,
+    "np": 0,
+    "nz": 0,
+    "pa": 0,
+    "pe": 0,
+    "ph": 0,
+    "pk": 0,
+    "pr": 0,
+    "py": 0,
+    "sg": 0,
+    "sv": 0,
+    "th": 0,
+    "tn": 0,
+    "tt": 0,
+    "tw": 0,
+    "um": 0,
+    "us": 0,
+    "ve": 0,
+    "vi": 0,
+    "ws": 0,
+    "za": 0,
+    "zw": 0
+  };
+
+  const country = locale.split("-")[1].toLowerCase();
+  const dow = firstDay[country];
+  return (dow === undefined) ? 1 : dow; /*Number*/
+}
+
+/**
+ * Floors the specified date to the beginning of week.
+ * @param {luxon.DateTime} date
+ * @returns {luxon.DateTime}
+ */
+const floorToWeek = (date) => {
+  console.log(date.locale)
+  const fd = getFirstDayOfWeek(date.locale);
+  const day = date.weekday % 7;	// convert to 0=sunday .. 6=saturday
+  const dayAdjust =  day >= fd ? -day + fd : -day + fd - 7;
+  return date.plus({days: dayAdjust});
+}
+
+
+// Main layout components
 
 const Toolbar = ({inputRef, onGoToDate, onInputBlur, onInputFocus, onMinus, onPlus}) => {
   const [value, setValue] = React.useState('')
@@ -235,7 +326,8 @@ const AlbumBrowser = ({rootFolder, expandedFolderIds, toggleFolder}) => {
   )
 }
 
-// Gallery Types
+
+// Gallery components
 
 const SquareThumbnailContents = ({photos, columns, setSelected}) => {
   return (
@@ -276,6 +368,52 @@ const VerticalStripeContents = ({photos, setSelected}) => {
   )
 }
 
+const CalendarContents = ({photos, setSelected}) => {
+  const sortedDates = photos
+    .map(p => p.date)
+    .sort((first, second) => first.localeCompare(second))
+
+  const firstWeek = floorToWeek(DateTime.fromFormat(sortedDates[0], "yyyy-MM-dd"))
+  const lastWeek = floorToWeek(DateTime.fromFormat(sortedDates[sortedDates.length - 1], "yyyy-MM-dd"))
+
+  // Build a map of "date: photo"
+  const photosMappedByDate = {}
+  groupBy(p => p.date, photos).forEach(({key, items}) => {
+    photosMappedByDate[key] = items[0] // take the first
+  })
+
+  const calendar = []
+  let weekCursor = firstWeek
+  while (weekCursor <= lastWeek) {
+    for (let i = 0; i <= 6; i++) {
+      const date = weekCursor.plus({ days: i })
+      const photo = photosMappedByDate[date.toFormat('yyyy-MM-dd')];
+      calendar.push({date, photo})
+    }
+
+    weekCursor = weekCursor.plus({ days: 7})
+  }
+
+  const renderDay = ({date, photo}, index) => {
+    const formatStr = (index === 0 || date.equals(date.startOf('month'))) ? 'LLL d' : 'd'
+    const photoElem = !photo ? <></> : <img src={`${THUMBNAILS_ROOT}/${photo.path}`} alt={photo.name} onClick={() => setSelected(photo)} />
+
+    const formattedDate = date.toFormat(formatStr);
+    return (
+      <div className="photo" key={formattedDate}>
+        <span className="date">{formattedDate}</span>
+        {photoElem}
+      </div>
+    )
+  }
+
+  return (
+    <div className="gallery gallery-7 calendar">
+      {calendar.map(renderDay)}
+    </div>
+  )
+}
+
 const AlbumContents = ({albums, columns}) => {
   let { albumId } = useParams();
 
@@ -287,10 +425,13 @@ const AlbumContents = ({albums, columns}) => {
     switch (galleryType) {
       case 'VerticalStripes':
         return VerticalStripeContents
+      case 'Calendar':
+        return CalendarContents
       case 'SquareThumbnails':
-        return SquareThumbnailContents;
+        return SquareThumbnailContents
       default:
-        throw Error(`Unknown gallery type "${galleryType}"`)
+        console.error(`Unknown gallery type "${galleryType}"`)
+        return SquareThumbnailContents
     }
   }
 
